a90c113b9d89d51f313b7e0f4b9ba2f644e42146
NoFoolLikeOne <nofool@paparomeo.uk>
2021-05-12 00:55:55 +0100

    Deployment Script

--- query/deploy.sh
+++ query/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcloud functions deploy query --allow-unauthenticated --runtime python39 --entry-point payload --trigger-http 
+gcloud functions deploy query --allow-unauthenticated --runtime python39 --entry-point payload --trigger-http  --max-instances 4 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD
