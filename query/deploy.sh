d992272fd0a1bdd57d8e63aa0e175fd7fb2850a1
NoFoolLikeOne <31373535+NoFoolLikeOne@users.noreply.github.com>
2021-05-12 12:28:31 +0100

    deploy local/remote
    This will be a standard deployment script to allow local or remote deployment. Save me having to force standardising the payloads.

--- query/deploy.sh
+++ query/deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-gcloud functions deploy query --allow-unauthenticated --runtime python39 --entry-point payload --trigger-http  --max-instances 4 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD
+entrypoint=payload
+
+if [[ "$1" == "local" ]] ; then
+    functions-framework  --debug --target $entrypoint --host $(hostname -I | awk '{print $1}')
+else
+    gcloud functions deploy query --allow-unauthenticated --runtime python39 --entry-point $entrypoint --trigger-http  --max-instances 4 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD
+fi
