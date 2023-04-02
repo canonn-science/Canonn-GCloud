cbd537d7b3936f3bd559f7ea1a19bb41d1485eee
NoFoolLikeOne <nofool@paparomeo.uk>
2023-04-02 12:19:12 +0000

    Extending timeout

--- postEvent/deploy.sh
+++ postEvent/deploy.sh
@@ -4,5 +4,5 @@ entrypoint=entrypoint
 if [[ "$1" == "local" ]] ; then
     functions-framework  --debug --target $entrypoint --host $(hostname -I | awk '{print $1}')
 else
-	gcloud functions deploy $(basename $(pwd)) --allow-unauthenticated --runtime python39 --entry-point $entrypoint --trigger-http  --max-instances 10 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD
+	gcloud functions deploy $(basename $(pwd)) --allow-unauthenticated --runtime python39 --entry-point $entrypoint --timeout 120 --trigger-http  --max-instances 10 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD
 fi
