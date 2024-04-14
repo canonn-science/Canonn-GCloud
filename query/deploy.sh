71b6c1c062bcaa822e32e53befe6bb0941d9f5e6
NoFoolLikeOne <nofool@paparomeo.uk>
2024-04-14 19:35:59 +0000

    Preparing for database migration

--- query/deploy.sh
+++ query/deploy.sh
@@ -2,7 +2,16 @@
 entrypoint=payload
 
 if [[ "$1" == "local" ]] ; then
+    INSTANCE_CONNECTION_NAME=canonn-api-236217:europe-north1:canonnpai ; export INSTANCE_CONNECTION_NAME
     functions-framework  --debug --target $entrypoint --host $(hostname -I | awk '{print $1}')
-else
-    gcloud functions deploy query --allow-unauthenticated --runtime python312 --entry-point $entrypoint --trigger-http  --max-instances 10 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD
+fi
+if [[ "$1" == "live" ]] ; then
+    gcloud functions deploy query --allow-unauthenticated --runtime python312 --entry-point $entrypoint --trigger-http  --max-instances 10 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD,INSTANCE_CONNECTION_NAME=canonn-api-236217:europe-north1:canonnpai
+fi
+if [[ "$1" == "test" ]] ; then
+    # Assumed to be using tunnel to the real test server on 3307
+    MYSQL_HOST=$(hostname -I | awk '{print $1}') ; export MYSQL_HOST
+    MYSQL_PORT=3307 ; export MYSQL_PORT
+    MYSQL_PASSWORD=$TEST_PASSWORD ; export MYSQL_PASSWORD
+    functions-framework  --debug --target $entrypoint --host $(hostname -I | awk '{print $1}')
 fi
--- query/localpackage/dbutils.py
+++ query/localpackage/dbutils.py
@@ -2,23 +2,22 @@ import pymysql
 from pymysql.err import OperationalError
 from os import getenv
 
-# TODO(developer): specify SQL connection details
-CONNECTION_NAME = getenv(
-    'INSTANCE_CONNECTION_NAME',
-    'canonn-api-236217:europe-north1:canonnpai')
-DB_USER = getenv('MYSQL_USER', 'canonn')
-DB_PASSWORD = getenv('MYSQL_PASSWORD', 'secret')
-DB_NAME = getenv('MYSQL_DATABASE', 'canonn')
-DB_HOST = getenv('MYSQL_HOST', 'localhost')
+CONNECTION_NAME = getenv("INSTANCE_CONNECTION_NAME", None)
+DB_USER = getenv("MYSQL_USER", "canonn")
+DB_PASSWORD = getenv("MYSQL_PASSWORD", "secret")
+DB_NAME = getenv("MYSQL_DATABASE", "canonn")
+DB_HOST = getenv("MYSQL_HOST", "localhost")
+DB_PORT = getenv("MYSQL_PORT", 3306)
 
 mysql_config = {
-    'user': DB_USER,
-    'password': DB_PASSWORD,
-    'db': DB_NAME,
-    'host': DB_HOST,
-    'charset': 'utf8mb4',
-    'cursorclass': pymysql.cursors.DictCursor,
-    'autocommit': True
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "db": DB_NAME,
+    "host": DB_HOST,
+    "port": int(DB_PORT),
+    "charset": "utf8mb4",
+    "cursorclass": pymysql.cursors.DictCursor,
+    "autocommit": True,
 }
 
 mysql_conn = None
@@ -37,16 +36,22 @@ def get_cursor():
         return mysql_conn.cursor()
 
 
+"""
+    It should only connect once
+    
+"""
+
+
 def setup_sql_conn():
     global mysql_conn
-    # Initialize connections lazily, in case SQL access isn't needed for this
-    # GCF instance. Doing so minimizes the number of active SQL connections,
-    # which helps keep your GCF instances under SQL connection limits.
     if not mysql_conn:
         try:
+            if CONNECTION_NAME is not None:
+                mysql_config["unix_socket"] = f"/cloudsql/{CONNECTION_NAME}"
             mysql_conn = pymysql.connect(**mysql_config)
         except OperationalError:
-            # If production settings fail, use local development ones
-            mysql_config['unix_socket'] = f'/cloudsql/{CONNECTION_NAME}'
+            # If we fail try again
+            if CONNECTION_NAME is not None:
+                mysql_config["unix_socket"] = f"/cloudsql/{CONNECTION_NAME}"
             mysql_conn = pymysql.connect(**mysql_config)
     mysql_conn.ping()
