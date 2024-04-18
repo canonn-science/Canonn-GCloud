7e672558cc889671cd23284d1d663dc722c00244
NoFoolLikeOne <nofool@paparomeo.uk>
2024-04-18 06:09:40 +0000

    Converting submitKills to python from Node

---
+++ submitKills/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+entrypoint=payload
+cd function
+
+if [[ "$1" == "local" ]] ; then
+    unset INSTANCE_CONNECTION_NAME
+    functions-framework  --debug --target $entrypoint --host $(hostname -I | awk '{print $1}')
+fi
+
+if [[ "$1" == "oldlive" ]] ; then
+    INSTANCE_CONNECTION_NAME=canonn-api-236217:europe-north1:canonnpai ; export INSTANCE_CONNECTION_NAME
+    gcloud functions deploy submitKills2  \
+        --allow-unauthenticated \
+        --runtime python312 \
+        --entry-point $entrypoint \
+        --trigger-http  \
+        --max-instances 10 \
+        --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD,INSTANCE_CONNECTION_NAME=canonn-api-236217:europe-north1:canonnpai
+fi
+
+if [[ "$1" == "test" ]] ; then
+    unset INSTANCE_CONNECTION_NAME
+    # we are using defaulst from the 
+    TUNNEL_KEY=$TUNNEL_KEY
+    TUNNEL_HOST=$TUNNEL_HOST
+    #We will set up a tunnel on 3308
+    #If the database connection is 3308 then the tunnel will route to the destination on 3306
+    #So MYSQL_PORT = local_port = 3308
+    MYSQL_HOST=localhost ; export MYSQL_HOST
+    MYSQL_PORT=3308 ; export MYSQL_PORT
+    MYSQL_PASSWORD=$TEST_PASSWORD ; export MYSQL_PASSWORD
+    functions-framework  --debug --target $entrypoint --host $(hostname -I | awk '{print $1}')
+fi
+
+if [[ "$1" == "live" ]] ; then
+    unset INSTANCE_CONNECTION_NAME
+    #We will set up a tunnel on 3308
+    #If the database connection is 3308 then the tunnel will route to the destination on 3306
+    #So MYSQL_PORT = local_port = 3308
+    gcloud functions deploy submitKills  \
+        --allow-unauthenticated \
+        --runtime python312 \
+        --entry-point $entrypoint \
+        --trigger-http  \
+        --max-instances 10 \
+        --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$TEST_PASSWORD,TUNNEL_KEY=$TUNNEL_KEY,TUNNEL_HOST=$TUNNEL_HOST,MYSQL_HOST=localhost,MYSQL_PORT=3308
+
+    fi
---
+++ submitKills/function/localpackage/dbutils.py
@@ -0,0 +1,61 @@
+import pymysql
+from pymysql.err import OperationalError
+from os import getenv
+import os
+
+
+CONNECTION_NAME = getenv("INSTANCE_CONNECTION_NAME", None)
+DB_USER = getenv("MYSQL_USER", "canonn")
+DB_PASSWORD = getenv("MYSQL_PASSWORD", "secret")
+DB_NAME = getenv("MYSQL_DATABASE", "canonn")
+DB_HOST = getenv("MYSQL_HOST", "localhost")
+DB_PORT = getenv("MYSQL_PORT", 3306)
+
+mysql_config = {
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "db": DB_NAME,
+    "host": DB_HOST,
+    "port": int(DB_PORT),
+    "charset": "utf8mb4",
+    "cursorclass": pymysql.cursors.DictCursor,
+    "autocommit": True,
+}
+
+mysql_conn = None
+
+
+def get_cursor():
+    """
+    Helper function to get a cursor
+      PyMySQL does NOT automatically reconnect,
+      so we must reconnect explicitly using ping()
+    """
+    try:
+        return mysql_conn.cursor()
+    except OperationalError:
+        mysql_conn.ping(reconnect=True)
+        return mysql_conn.cursor()
+
+
+"""
+    It should only connect once
+    
+"""
+
+
+def setup_sql_conn():
+    global mysql_conn
+    if not mysql_conn:
+        try:
+            if CONNECTION_NAME is not None:
+                mysql_config["unix_socket"] = f"/cloudsql/{CONNECTION_NAME}"
+            mysql_conn = pymysql.connect(**mysql_config)
+        except OperationalError:
+            # If we fail try again
+            if CONNECTION_NAME is not None:
+                mysql_config["unix_socket"] = f"/cloudsql/{CONNECTION_NAME}"
+            mysql_conn = pymysql.connect(**mysql_config)
+    else:
+        print("already connected to mysql")
+    mysql_conn.ping()
