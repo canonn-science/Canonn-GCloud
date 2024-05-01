ef2069563b5f047b48174499eec997c07843d16d
NoFoolLikeOne <nofool@paparomeo.uk>
2024-05-01 16:58:02 +0000

    python 312

--- postHDDetected/deploy.sh
+++ postHDDetected/deploy.sh
@@ -8,11 +8,11 @@ if [[ "$1" == "local" ]] ; then
 fi
 
 if [[ "$1" == "live" ]] ; then
-    gcloud functions deploy postHDDetected --region "europe-west1" --allow-unauthenticated --runtime python39 --entry-point $entrypoint --trigger-http  --max-instances 2 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD,INSTANCE_CONNECTION_NAME=$INSTANCE_CONNECTION_NAME,MYSQL_DATABASE=$MYSQL_DATABASE
+    gcloud functions deploy postHDDetected --region "europe-west1" --allow-unauthenticated --runtime python312 --entry-point $entrypoint --trigger-http  --max-instances 2 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD,INSTANCE_CONNECTION_NAME=$INSTANCE_CONNECTION_NAME,MYSQL_DATABASE=$MYSQL_DATABASE
 fi
 
 if [[ "$1" == "test" ]] ; then    
-    gcloud functions deploy postHDDetected2 --region "europe-west1" --allow-unauthenticated --runtime python39 --entry-point $entrypoint --trigger-http  --max-instances 2 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD,INSTANCE_CONNECTION_NAME=$INSTANCE_CONNECTION_NAME,MYSQL_DATABASE=$MYSQL_DATABASE
+    gcloud functions deploy postHDDetected2 --region "europe-west1" --allow-unauthenticated --runtime python312 --entry-point $entrypoint --trigger-http  --max-instances 2 --set-env-vars=MYSQL_USER=$MYSQL_USER,MYSQL_PASSWORD=$MYSQL_PASSWORD,INSTANCE_CONNECTION_NAME=$INSTANCE_CONNECTION_NAME,MYSQL_DATABASE=$MYSQL_DATABASE
 fi
 
 
